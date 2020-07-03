00038517549cb2f4f6e7ee7a2944a940624b0034
Toshio Watanabe <toshio_ws@hotmail.com>
2020-07-03 20:04:11 -0300

    interceptor

---
+++ src/br/com/caelum/contas/controller/LoginController.java
@@ -0,0 +1,31 @@
+package br.com.caelum.contas.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.caelum.contas.dao.UsuarioDAO;
+import br.com.caelum.contas.modelo.Usuario;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("/login")
+	public String formLogin() {
+		return "usuario/formLogin";
+	}
+	
+	@RequestMapping("/efetuaLogin")
+	public String login(Usuario usuario, HttpSession session) {
+		
+		UsuarioDAO dao = new UsuarioDAO();
+		
+		if(dao.existeUsuario(usuario)) {
+			session.setAttribute("usuarioLogado", usuario);
+			return "menu-login";
+		}
+		
+		return "redirect:/login";
+	}
+}
--- src/br/com/caelum/contas/interceptor/AutorizadorInterceptor.java
+++ src/br/com/caelum/contas/interceptor/AutorizadorInterceptor.java
@@ -5,22 +5,27 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class AutorizadorInterceptor extends HandlerInterceptorAdapter {
 
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object controller)
-	throws Exception {
+public class AutorizadorInterceptor extends HandlerInterceptorAdapter {
+				
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+			Object controller) throws Exception{
 		
 		String uri = request.getRequestURI();
-		if(uri.endsWith("loginForm") || uri.endsWith("efetuaLogin") || uri.contains("resources")) {
+		
+		if(uri.endsWith("login") || uri.endsWith("efetuaLogin") || 
+				uri.endsWith("resources")) {
+			
 			return true;
 		}
 		
-		if(request.getSession().getAttribute("usuarioLogado")!=null) {
+		if(request.getSession().getAttribute("usuarioLogado") != null) {
 			return true;
-		} else {
-			response.sendRedirect("loginForm");
+		}
+		
+		else {
+			response.sendRedirect("login");
 			return false;
 		}
-		
 	}
 }
